534cc2b0ec890b466638d128c5ba1d5501650e42
R-enanVieira <renanv.guedes7@gmail.com>
2024-12-22 11:45:33 -0300

    feat(docs): add random numbers generator

--- docs/modules/numbers.js
+++ docs/modules/numbers.js
@@ -1,30 +1,37 @@
 import { Module } from "../module.js"
 
-
+/**
+ * @param {number} min
+ * @param {number} max
+ * @param {number} amount
+ * @returns {Array<number>} 
+ */
+function generateRandomNumbers(min, max, amount) {
+    const numbers = new Set()
+    while (numbers.size < amount) {
+        const randomNum = Math.floor(Math.random() * (max - min + 1)) + min
+        numbers.add(randomNum)
+    }
+    return Array.from(numbers)
+}
 
 /**
-* @param {"easy" | string} difficulty 
+* @param {"tutorial" | "easy" | "mid" | "hard" | string} difficulty 
 * @returns Array<string>
 */
 function render(difficulty) {
     switch (difficulty) {
         case "tutorial":
-            return // 2 ~ 3 numbers between 1 and 10
-        break;
-
+            return generateRandomNumbers(1, 10, Math.floor(Math.random() * (3 - 2 + 1)) + 2);// 2 ~ 3 numbers between 1 and 10
         case "easy":
-            return // 4 ~ 9 numbers between 1 and 15
-        break;
-
+            return generateRandomNumbers(1, 15, Math.floor(Math.random() * (9 - 4 + 1)) + 4) // 4 ~ 9 numbers between 1 and 15
         case "mid":
-            return // 10 ~ 9 numbers between 1 and 30
-        break;
-
+            return  generateRandomNumbers(1, 30, Math.floor(Math.random() * (19 - 10 + 1)) + 10) // 10 ~ 9 numbers between 1 and 30
         case "hard":
-            return // 20 ~ 30 numbers between 1 and 50
-        break;
+            return generateRandomNumbers(1, 50, Math.floor(Math.random() * (30 - 20 + 1)) + 20) // 20 ~ 30 numbers between 1 and 50
+        default:
+            return []
     }
-    return []
 }
 
 export const Japanese = new Module(
